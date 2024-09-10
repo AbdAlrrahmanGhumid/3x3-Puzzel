bfce5f155c88e45ae19695f1ad2632c59d7040e0
AbdAlrrahmanGhumid <abbode.gho@gmail.com>
2024-09-10 15:35:16 +0200

    first version. moving the number by giving it through the console using user input.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Template template = new Template(); // an instance of the 3x3 puzzle is initilised
+
+        while (!template.won) { // as long as the game is not won
+            template.viewPuzzle();
+            try { // try-catch to avoid invalid input: i < 1, i >8 , strings
+                template.move(getUserInput()); // getUserInput ask the user to enter a number
+            } catch (Exception e) {
+                System.out.println("invalid input!");
+            }
+            template.checkWinning();
+        }
+        
+        template.viewPuzzle();
+        System.out.println("YOU WON THE GAME!");    
+
     }
+
+    public static int getUserInput() {
+        Scanner myObj = new Scanner(System.in); // Create a Scanner object
+        System.out.println("Enter Puzzle Number to Move");
+        String input = myObj.nextLine(); // Read user input
+        return Integer.valueOf(input);
+    }
+
 }
---
+++ src/Template.java
@@ -0,0 +1,203 @@
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class Template {
+    public Template() {
+        do {
+            generateRandomPuzzle();
+        } while (!isSolvable());
+
+    }
+
+    Integer rightNeighbour, leftNeighbour, upperNeighbour, lowerNeighbour; // these variables are not the true
+                                                                           // neighbours however their positions in the
+                                                                           // array
+    Set<Integer> numbers = new HashSet<>();
+    private Integer[] content = new Integer[9];
+    private int nullPosition = position(null);
+    public boolean won = false;
+
+    public Integer pickUpRandomElemnt() {
+        int size = numbers.size();
+        int randomNumber = new Random().nextInt(size);
+        int i = 0;
+        for (Integer number : numbers) {
+            if (i == randomNumber) {
+                numbers.remove(number);
+                return number;
+            }
+            i++;
+        }
+        return null;
+    }
+
+    private boolean isSolvable() { // tests if the randomly generated puzzle is solveable
+        return (countInversions() % 2 == 0); // solvable puzzles have even number of inversion
+    }
+
+    private int countInversions() { // one inversion for each pair of tiles (a, b), if a > b and a comes before b
+        int inversions = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                
+                if (content[i]!= null && content[j]!= null && content[i] > content[j]) {
+                    inversions++;
+                }
+            }
+        }
+        return inversions;
+    }
+
+    private void generateRandomPuzzle() {
+        numbers.clear();
+        for (int i = 1; i <= 8; i++) { // numbers 1 to 8 are added to the set numbers
+            numbers.add(i);
+        }
+        numbers.add(null); // element null is added to have an empty field in the puzzle
+        for (int i = 0; i < 9; i++) {
+            content[i] = pickUpRandomElemnt(); // the elements of set numbers are put randomly in the arry content to
+                                               // produce random puzzle
+        }
+        updateNullPosition();
+        updateNeighbours();
+    }
+
+    public void viewPuzzle() {
+        System.out.printf("|-------|-------|-------|%n");
+        for (int i = 0; i < 9; i = i + 3) {
+            System.out.printf("|%4s   |%4s   |%4s   | %n",
+                    this.content[i] != null ? this.content[i].toString() : "",
+                    this.content[i + 1] != null ? this.content[i + 1].toString() : "",
+                    this.content[i + 2] != null ? this.content[i + 2].toString() : "");
+            System.out.printf("|-------|-------|-------|%n");
+        }
+    }
+
+    public void move(int number) {
+        move(checkNeighbourhood(number));
+    }
+
+    private String checkNeighbourhood(int number) { // try-catch is used because Neighbour might be a null element
+        try {
+            if (number == content[rightNeighbour])
+                return "Right";
+        } catch (NullPointerException e) {
+        }
+        try {
+            if (number == content[leftNeighbour])
+                return "Left";
+        } catch (NullPointerException e) {
+        }
+        try {
+            if (number == content[upperNeighbour])
+                return "Up";
+        } catch (Exception e) {
+        }
+        try {
+            if (number == content[lowerNeighbour])
+                return "Down";
+        } catch (Exception e) {
+        }
+
+        return null;
+    }
+
+    public void move(String direction) { // the method move the null element in the given direction
+        int row = nullPosition / 3; // the variables row and column are useful hinder the movement takes the null
+                                    // element outside of the puzzle. (outside means graphicly)
+        int column = nullPosition % 3;
+        switch (direction) { // the positions of the null elmemnt and its according neighbour are switched
+                             // with the usage of help varaible. after that the new positions are updated.
+            case "Right":
+                if (column < 2) {
+                    Integer helpMemory = content[rightNeighbour];
+                    content[nullPosition] = helpMemory;
+                    content[rightNeighbour] = null;
+                    updateNullPosition();
+                    updateNeighbours();
+                }
+                break;
+            case "Left":
+                if (column > 0) {
+                    Integer helpMemory = content[leftNeighbour];
+                    content[nullPosition] = helpMemory;
+                    content[leftNeighbour] = null;
+                    updateNullPosition();
+                    updateNeighbours();
+                }
+                break;
+            case "Up":
+                if (row > 0) {
+                    Integer helpMemory = content[upperNeighbour];
+                    content[nullPosition] = helpMemory;
+                    content[upperNeighbour] = null;
+                    updateNullPosition();
+                    updateNeighbours();
+                }
+                break;
+            case "Down":
+                if (row < 2) {
+                    Integer helpMemory = content[lowerNeighbour];
+                    content[nullPosition] = helpMemory;
+                    content[lowerNeighbour] = null;
+                    updateNullPosition();
+                    updateNeighbours();
+                }
+                break;
+        }
+
+    }
+
+    private void updateNullPosition() {
+        nullPosition = position(null);
+    }
+
+    private void updateNeighbours() {
+        int row = nullPosition / 3; // the two variables help in giving the null element the right neighbours. if
+                                    // the null elment is sitting on the right edge, the rightNeighbour is null.
+        int column = nullPosition % 3;
+
+        if (column < 2)
+            rightNeighbour = nullPosition + 1;
+        else
+            rightNeighbour = null;
+
+        if (column > 0)
+            leftNeighbour = nullPosition - 1;
+        else
+            leftNeighbour = null;
+
+        if (row > 0)
+            upperNeighbour = nullPosition - 3;
+        else
+            upperNeighbour = null;
+
+        if (row < 2)
+            lowerNeighbour = nullPosition + 3;
+        else
+            lowerNeighbour = null;
+    }
+
+    private int position(Integer number) { // gives the position of the input number in the array.
+        int position = 0;
+        for (Integer i : content) {
+            if (i == number)
+                return position;
+            position++;
+        }
+        return 0;
+    }
+
+    public void checkWinning() { // check if the array is arranged coorectly.
+        won = true;
+        for (int i = 0; i < 8; i++) {
+            if (content[i] == null ||content[i] != i + 1) {
+                won = false;
+                break;
+            }
+        }
+    }
+
+}
